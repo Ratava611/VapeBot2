c7efe72f69e41997b506d6012d5b59cd7050c7f3
ratava611 <ratava611@gmail.com>
2020-09-30 12:41:29 -0400

    Added ping command

--- src/main/java/bot/EventListener.java
+++ src/main/java/bot/EventListener.java
@@ -1,6 +1,6 @@
 package bot;
 
-import net.dv8tion.jda.api.JDA;
+//import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -22,11 +22,12 @@ public class EventListener extends ListenerAdapter {
     /**
      * Fields
      */
-    private CommandManager manager = new CommandManager();
+    private final CommandManager manager = new CommandManager(); // Initializes command parser
     private final Logger logger = LoggerFactory.getLogger(EventListener.class);
 
     /**
-     * Constructor
+     * When this is fired all available entities are cached and accessible.
+     * @param event Indicates that JDA finished loading all entities.
      */
     @Override
     public void onReady(ReadyEvent event)
@@ -46,25 +47,25 @@ public class EventListener extends ListenerAdapter {
         String msg = message.getContentDisplay();       //This is the MessageChannel that the message was sent to.
 
         //These are provided with every event in JDA
-        JDA jda = event.getJDA();
-        long responseNumber = event.getResponseNumber();
+        //JDA jda = event.getJDA();
+        //long responseNumber = event.getResponseNumber();
 
-
-        if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        if (event.isFromType(ChannelType.PRIVATE) && !author.isBot()) //If this message was sent to a PrivateChannel
         {
-            PrivateChannel privateChannel = event.getPrivateChannel();
-
+            event.getChannel().sendMessage("This hasn't been implemented yet. But thanks for checking in!").queue();
             logger.info(String.format("[PRIV]<%s>: %s", author.getName(), msg));
         }
         else if (event.isFromType(ChannelType.TEXT))   //If this message was sent to a Group. This is CLIENT only!
         {
-            Guild guild = event.getGuild();
             TextChannel textChannel = event.getTextChannel();
-
-            logger.info(String.format("[GRP: %s]<%s>: %s", guild.getName(), textChannel.getName(), author, msg));
+            logger.info(String.format("[GRP: %s]<%s>: %s", textChannel.getName(), author, msg));
         }
     }
 
+    /**
+     * Passes prefixed commands to the command manager
+     * @param event on any server channel message received event that we have permissions in
+     */
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event)
     {
